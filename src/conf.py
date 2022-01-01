86fb4f99e8ccdda5762f9c54cd24a85123064355
ousttrue <ousttrue@gmail.com>
2022-01-01 14:29:49 +0900

    grid layout

--- src/conf.py
+++ src/conf.py
@@ -44,14 +44,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_book_theme'
-html_theme = 'bizstyle'
-# html_theme = 'custom'
-# html_theme_path = ['.']
+html_theme = 'custom'
+html_theme_path = ['.']
 html_sidebars = {
     '**': ['searchbox.html', 'relations.html', 'customtoc.html', 'localtoc.html'],
 }
-# html_style = 'my.css'
 
 
 def setup(app):
